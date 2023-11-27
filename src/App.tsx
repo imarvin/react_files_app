7ca98cfc6f6b4cc3aeb26bd490c08678bc431df9
Marvin Orendain <marvin.orendain@elis-mac-mini.mynetworksettings.com>
2023-11-27 13:58:43 -0500

    update app to use FilesTable with new CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import { filesData } from './data';
+import FilesTable from './components/organisms/FilesTable';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>React File App</h1>
       </header>
+      <FilesTable files={filesData} />
     </div>
   );
 }
