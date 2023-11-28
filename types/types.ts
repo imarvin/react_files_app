dd68d236731778b11761d894c0c1ce062742d2a2
Marvin Orendain <marvin.orendain@elis-mac-mini.mynetworksettings.com>
2023-11-28 09:16:49 -0500

    updates types

--- types/types.ts
+++ types/types.ts
@@ -1,6 +1,18 @@
-export type File = {
+type File = {
   name: string;
   device: string;
   path: string;
   status: string;
+  checked?: boolean;
 };
+
+type ContextType = {
+  files: File[];
+  setFiles: any;
+  selected: number[];
+  setSelected: any;
+  isModalOpen: boolean;
+  setModalOpen: any;
+};
+
+export type { File, ContextType };
